f9e2cd5792734c82c197c420bc15276a8b605c3d
ishanthewizard <77704916+ishanthewizard@users.noreply.github.com>
2023-10-04 02:25:01 -0700

    Finished mos tof content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
+import DocumentList from "@/components/DocumentList"
 import MainTitle from "@/components/MainTitle"
 import Navbar from "@/components/Navbar"
 import { VStack } from "@chakra-ui/react"
 export default function Page() {
+ const documents = ['Document 1', 'Document 2', 'Document 3', "bfdafasfdasfasfsa", "fafdasfdasfasfasfsaas", "fdafasfasfsafsavcxads", "fdasfasfasfdasf"];
   return (
 <VStack m="10px" width="100%">
     <Navbar/>
     <MainTitle />
+    <DocumentList documents={documents}/>
 </VStack>
   
   )
